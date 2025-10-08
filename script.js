8ec3d88a89c88f856e85508d0b6dc5e34430829c
minorlu <scherbodenis@gmail.com>
2025-10-08 19:29:34 +0300

    Add game main logic

--- script.js
+++ script.js
@@ -1,17 +1,59 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
 
     if (computerChoice === 1) {
-        return "Rock";
+        return "rock";
     } else if (computerChoice === 2) {
-        return "Paper";
+        return "paper";
     } else {
-        return "Scissors";
+        return "scissors";
     }
 }
 
 function getHumanChoice() {
     let userChoice = prompt("Enter your choice:");
-    return userChoice;
+    return userChoice.toLowerCase();
 }
 
+function playRound(humanChoice, computerChoice) {
+    console.log("The computer selected " + computerChoice)
+    if (humanChoice === "rock") {
+        if (computerChoice === "rock") {
+            console.log("Its tie!");
+        } else if (computerChoice === "paper") {
+            console.log("You lose!");
+            computerScore++;
+        } else {
+            console.log("You win!");
+            humanScore++;
+        }
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            console.log("You win!");
+            humanScore++;
+        } else if (computerChoice === "paper") {
+            console.log("Its tie!");
+        } else {
+            console.log("You lose!");
+            computerScore++;
+        }
+    } else {
+        if (computerChoice === "rock") {
+            console.log("You lose!");
+            computerScore++;
+        } else if (computerChoice === "paper") {
+            console.log("You win!");
+            humanScore++;
+        } else {
+            console.log("Its tie!");
+        }
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
