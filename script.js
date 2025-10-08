036810a025d49371a1c045fc5ea8b3a991c54bb4
minorlu <scherbodenis@gmail.com>
2025-10-08 19:35:30 +0300

    Add game main function

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
 
@@ -18,42 +15,62 @@ function getHumanChoice() {
     return userChoice.toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-    console.log("The computer selected " + computerChoice)
-    if (humanChoice === "rock") {
-        if (computerChoice === "rock") {
-            console.log("Its tie!");
-        } else if (computerChoice === "paper") {
-            console.log("You lose!");
-            computerScore++;
+
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        console.log("The computer selected " + computerChoice)
+        if (humanChoice === "rock") {
+            if (computerChoice === "rock") {
+                console.log("Its tie!");
+            } else if (computerChoice === "paper") {
+                console.log("You lose!");
+                computerScore++;
+            } else {
+                console.log("You win!");
+                humanScore++;
+            }
+        } else if (humanChoice === "paper") {
+            if (computerChoice === "rock") {
+                console.log("You win!");
+                humanScore++;
+            } else if (computerChoice === "paper") {
+                console.log("Its tie!");
+            } else {
+                console.log("You lose!");
+                computerScore++;
+            }
         } else {
-            console.log("You win!");
-            humanScore++;
+            if (computerChoice === "rock") {
+                console.log("You lose!");
+                computerScore++;
+            } else if (computerChoice === "paper") {
+                console.log("You win!");
+                humanScore++;
+            } else {
+                console.log("Its tie!");
+            }
         }
-    } else if (humanChoice === "paper") {
-        if (computerChoice === "rock") {
-            console.log("You win!");
-            humanScore++;
-        } else if (computerChoice === "paper") {
-            console.log("Its tie!");
-        } else {
-            console.log("You lose!");
-            computerScore++;
-        }
+    }
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+        console.log("Score:\nComputer  " + computerScore + ":" + humanScore + "  Human");
+    }
+
+    if (computerScore > humanScore) {
+        console.log("GAME OVER: YOU LOST!!!");
+    } else if (humanScore > computerScore) {
+        console.log("WOOHOOO!!! YOU WON THE GAME!!!");
     } else {
-        if (computerChoice === "rock") {
-            console.log("You lose!");
-            computerScore++;
-        } else if (computerChoice === "paper") {
-            console.log("You win!");
-            humanScore++;
-        } else {
-            console.log("Its tie!");
-        }
+        console.log("WELL, IT'S TIE");
     }
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
