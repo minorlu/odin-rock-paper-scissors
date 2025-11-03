c2f874c22267745057a28a331945e78fb85218d2
minorlu <scherbodenis@gmail.com>
2025-11-03 17:39:36 +0300

    Add score display and round limits

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
 
+let endGameFlag = false;
+
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3) + 1;
 
@@ -26,8 +28,17 @@ function playGame() {
     let computerScore = 0;
 
     const resultDiv = document.querySelector(".results");
+    const score = document.querySelector("#score");
 
     function playRound(humanChoice, computerChoice) {
+        if (endGameFlag) {
+            humanScore = 0;
+            computerScore = 0;
+            endGameFlag = false;
+            resultDiv.innerHTML = "";
+            score.innerHTML = "0:0";
+        }
+
         resultDiv.innerHTML += "The computer selected " + computerChoice + "<br>";
         
         if (humanChoice === "rock") {
@@ -61,27 +72,21 @@ function playGame() {
                 resultDiv.innerHTML += "Its tie!<br><br>";
             }
         }
+
+        score.innerHTML = `${computerScore}:${humanScore}`;
+
+        if (humanScore == 5) {
+            resultDiv.innerHTML = "YOU WON THE GAME";
+            endGameFlag = true;
+        } else if (computerScore == 5) {
+            resultDiv.innerHTML = "YOU LOST";
+            endGameFlag = true;
+        }
     }
 
     rockBtn.addEventListener("click", () => playRound("rock", getComputerChoice()));
     paperBtn.addEventListener("click", () => playRound("paper", getComputerChoice()));
     scissorsBtn.addEventListener("click", () => playRound("scissors", getComputerChoice()));
-    
-    // for (let i = 0; i < 5; i++) {
-    //     const humanSelection = getHumanChoice();
-    //     const computerSelection = getComputerChoice();
-
-    //     playRound(humanSelection, computerSelection);
-    //     console.log("Score:\nComputer  " + computerScore + ":" + humanScore + "  Human");
-    // }
-
-    // if (computerScore > humanScore) {
-    //     console.log("GAME OVER: YOU LOST!!!");
-    // } else if (humanScore > computerScore) {
-    //     console.log("WOOHOOO!!! YOU WON THE GAME!!!");
-    // } else {
-    //     console.log("WELL, IT'S TIE");
-    // }
 }
 
 playGame();
